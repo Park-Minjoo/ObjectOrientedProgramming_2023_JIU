9b55cef67f0cf95198060c6e7f6cf98d4e09d7b1
parkminjoo <alswn9030@gmila.com>
2023-01-19 16:03:59 +0700

    [UPDATE] practice

---
+++ Practice/Chap2/Problems/KilometerperMile.java
@@ -0,0 +1,15 @@
+package Problems;
+
+import java.util.Scanner;
+public class KilometerperMile {
+    public static void main(String[] args) {
+//        double miles = 100;
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter a number for miles: ");
+      double miles = input.nextDouble();
+      final double kilometerPerMile = 1.609;
+      double kilometers = miles * kilometerPerMile;
+
+      System.out.println("Kilometers: " + kilometers);
+    }
+}
