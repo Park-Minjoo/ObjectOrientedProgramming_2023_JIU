911eb3325fe040dfeeabb9d345edaa12dabe94b7
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-19 14:17:46 +0700

    [UPDATE] Chap2

---
+++ Practice/Chap02/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ComputeAreaWithConsoleInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number for radius: ");
+    }
+}
