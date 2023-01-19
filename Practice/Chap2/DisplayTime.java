9a849ed050b731d6cb479fd7825f83f4b13cdc73
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-19 15:30:19 +0700

    [UPDATE] chap2

---
+++ Practice/Chap2/DisplayTime.java
@@ -0,0 +1,14 @@
+package Chap2;
+import java.util.Scanner;
+public class DisplayTime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Prompt the user for input
+        System.out.println("Enter an integer for seconds: ");
+        int seconds = input.nextInt();
+
+        int minutes = seconds / 60; // Find minutes in seconds
+        int remainingSeconds = seconds % 60; // Seconds remaining
+        System.out.println(seconds + " second is " + minutes + " minutes and " + remainingSeconds + " seconds");
+    }
+}
