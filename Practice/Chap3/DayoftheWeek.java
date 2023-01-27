3e41c7d3eb3a8c354f125988cb866991856eabd0
Park-Minjoo <alswn9030@gmail.com>
2023-01-27 10:46:58 +0700

    [UPLOAD] DayoftheWeek

--- Practice/Chap3/DayoftheWeek.java
+++ Practice/Chap3/DayoftheWeek.java
@@ -9,33 +9,33 @@ public class DayoftheWeek {
         System.out.print("Enter the number of days elapsed since today: ");
         int elapsed = input.nextInt();
 
-        int future_date = (date + elapsed) % 7;
-        String day_of_week = "";
+        int futureDate = (date + elapsed) % 7;
+        String dayoftheWeek = "";
 
         switch(date){
-            case 0: day_of_week = "Sunday"; break;
-            case 1: day_of_week = "Monday"; break;
-            case 2: day_of_week = "Tuesday"; break;
-            case 3: day_of_week = "Wednesday"; break;
-            case 4: day_of_week = "Thursday"; break;
-            case 5: day_of_week = "Friday"; break;
-            case 6: day_of_week = "Saturday"; break;
+            case 0: dayoftheWeek = "Sunday"; break;
+            case 1: dayoftheWeek = "Monday"; break;
+            case 2: dayoftheWeek = "Tuesday"; break;
+            case 3: dayoftheWeek = "Wednesday"; break;
+            case 4: dayoftheWeek = "Thursday"; break;
+            case 5: dayoftheWeek = "Friday"; break;
+            case 6: dayoftheWeek = "Saturday"; break;
         }
 
-        if (future_date == 0){
-            System.out.printf("Todays is %s and the future day is Sunday", day_of_week);
-        }else if(future_date == 1){
-            System.out.printf("Todays is %s and the future day is Monday", day_of_week);
-        }else if(future_date == 2){
-            System.out.printf("Todays is %s and the future day is Tuesday", day_of_week);
-        }else if(future_date == 3){
-            System.out.printf("Todays is %s and the future day is Wednesday", day_of_week);
-        }else if(future_date == 4){
-            System.out.printf("Todays is %s and the future day is Thursday", day_of_week);
-        }else if(future_date == 5){
-            System.out.printf("Todays is %s and the future day is Friday", day_of_week);
-        }else if(future_date == 6) {
-            System.out.printf("Todays is %s and the future day is Saturday", day_of_week);
+        if (futureDate == 0){
+            System.out.printf("Todays is %s and the future day is Sunday", dayoftheWeek);
+        }else if(futureDate == 1){
+            System.out.printf("Todays is %s and the future day is Monday", dayoftheWeek);
+        }else if(futureDate == 2){
+            System.out.printf("Todays is %s and the future day is Tuesday", dayoftheWeek);
+        }else if(futureDate == 3){
+            System.out.printf("Todays is %s and the future day is Wednesday", dayoftheWeek);
+        }else if(futureDate == 4){
+            System.out.printf("Todays is %s and the future day is Thursday", dayoftheWeek);
+        }else if(futureDate == 5){
+            System.out.printf("Todays is %s and the future day is Friday", dayoftheWeek);
+        }else if(futureDate == 6) {
+            System.out.printf("Todays is %s and the future day is Saturday", dayoftheWeek);
         }
     }
 }
