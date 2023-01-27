065eb9f70ee10557d2c50a81781a0663407f979b
Park-Minjoo <alswn9030@gmail.com>
2023-01-27 10:14:24 +0700

    [UPDATE] Switch

--- Practice/Chap3/Switch.java
+++ Practice/Chap3/Switch.java
@@ -1,50 +1,23 @@
+import java.util.Scanner;
+
 public class Switch {
 
     public static void main(String[] args) {
-        System.out.println("switch(1)");
-        switch(1) {
-            case 1:
-                System.out.println("one");
-                break;
-            case 2:
-                System.out.println("two");
-                break;
-            case 3:
-                System.out.println("three");
-                break;
-            default:
-                System.out.println("default");
-                break;
-        }
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
 
-//        System.out.println("switch(2)");
-//        switch(2) {
-//            case 1:
-//                System.out.println("one");
-//                break;
-//            case 2:
-//                System.out.println("two");
-//                break;
-//            case 3:
-//                System.out.println("three");
-//                break;
-//            default:
-//                System.out.println("default");
-//                break;
-//        }
-//
-//        System.out.println("switch(3)");
-//        switch(3) {
-//            case 1:
-//                System.out.println("one");
-//            case 2:
-//                System.out.println("two");
-//            case 3:
-//                System.out.println("three");
-//            default:
-//                System.out.println("default");
-//                break;
-//        }
+        String numberStr = null;
+        switch (number) {
+            case 0:  numberStr = "Sunday";    break;
+            case 1:  numberStr = "Monday";    break;
+            case 2:  numberStr = "Tuesday";   break;
+            case 3:  numberStr = "Wednesday"; break;
+            case 4:  numberStr = "Thursday";  break;
+            case 5:  numberStr = "Friday";    break;
+            case 6:  numberStr = "Saturday";  break;
+            default: numberStr = "Not a valid day"; break;
+        }
+        System.out.println(numberStr);
     }
 
 }
