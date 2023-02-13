de7ae407ee4c61cd54f8c9f72e3549743fefa698
Park-Minjoo <alswn9030@gmail.com>
2023-02-13 16:51:14 +0700

    [UPDATE] Boolean

---
+++ Practice/Chap5/Boolean.java
@@ -0,0 +1,6 @@
+public class Boolean {
+    public static void main(String[] args) {
+        System.out.println(1 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1 == 0.5);
+        System.out.println(1 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1);
+    }
+}
