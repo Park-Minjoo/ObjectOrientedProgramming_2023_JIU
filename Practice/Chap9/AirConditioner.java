22a1a23ee8e854b7dfa6f20add7060c62aadc476
Park-Minjoo <alswn9030@gmail.com>
2023-04-03 14:30:58 +0700

    [UPDATE] Class Assignment

---
+++ Practice/Chap9/AirConditioner.java
@@ -0,0 +1,11 @@
+public class AirConditioner {
+    static final int LOW = 1;
+    static final int MEDIUM = 2;
+    static final int HIGH = 3;
+
+    private int temperature = 72;
+    private int speed = LOW;
+    private boolean on = false;
+
+
+}
