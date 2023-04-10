541c3c33c158a176a4db28c140b89321fa2173be
Park-Minjoo <alswn9030@gmail.com>
2023-04-10 20:25:33 +0700

    [UPDATE] inheritance example - animal

---
+++ Practice/Chap11/Animal.java
@@ -0,0 +1,18 @@
+class Animal {
+    public void makeSound() {
+        System.out.println("Some sound");
+    }
+}
+
+class Cat extends Animal {
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
+class Dog extends Animal {
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
+
