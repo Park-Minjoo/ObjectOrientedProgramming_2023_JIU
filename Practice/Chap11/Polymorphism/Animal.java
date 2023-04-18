8e13ef2ab000e940b47c38700b56e36afac3822c
Park-Minjoo <alswn9030@gmail.com>
2023-04-18 16:02:40 +0700

    [UPDATE] constructors in Dog class

--- Practice/Chap11/Polymorphism/Animal.java
+++ Practice/Chap11/Polymorphism/Animal.java
@@ -11,12 +11,16 @@ class Animal {
         this.sound = sound;
         this.name = name;
     }
+
     public void makeSound() {
         System.out.println("The animal makes a sound");
     }
 }
 
 class Dog extends Animal {
+    Dog(String name, String sound){
+        super(sound, name);
+    }
     @Override
     public void makeSound() {
         System.out.println("The dog barks");
