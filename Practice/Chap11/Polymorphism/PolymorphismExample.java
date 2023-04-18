ee3f133bdf78fd66f2fe49e278351ae021efb44c
Park-Minjoo <alswn9030@gmail.com>
2023-04-18 16:21:21 +0700

    [UPDATE] PolymorphismExample

--- Practice/Chap11/Polymorphism/PolymorphismExample.java
+++ Practice/Chap11/Polymorphism/PolymorphismExample.java
@@ -3,6 +3,7 @@ package Polymorphism;
 public class PolymorphismExample {
     public static void main(String[] args) {
         Animal animal1 = new Animal();
+        Dog dog1 = new Dog();
         Animal animal2 = new Dog();
         Animal animal3 = new Cat();
 
