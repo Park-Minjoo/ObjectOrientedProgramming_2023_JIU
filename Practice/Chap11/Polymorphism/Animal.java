9fe95d6af8bc436f1d8009d93a3f73a319b99778
Park-Minjoo <alswn9030@gmail.com>
2023-04-18 15:55:43 +0700

    [UPDATE] constructors

--- Practice/Chap11/Polymorphism/Animal.java
+++ Practice/Chap11/Polymorphism/Animal.java
@@ -3,6 +3,14 @@ package Polymorphism;
 class Animal {
     private String sound;
     private String name;
+
+    Animal(){
+
+    }
+    Animal(String sound, String name){
+        this.sound = sound;
+        this.name = name;
+    }
     public void makeSound() {
         System.out.println("The animal makes a sound");
     }
